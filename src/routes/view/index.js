32df86f7d0e2686423878af8f58f40364db7fb4d
Leon <leonwheeler08@gmail.com>
2021-06-28 17:19:08 +0100

    Added redirection for bad routes.

--- src/routes/view/index.js
+++ src/routes/view/index.js
@@ -4,10 +4,12 @@ const authenticate = require("../../middleware/authenticate");
 
 const publicViews = require("./publicViews");
 const privateViews = require("./privateViews");
+const { renderHomePage } = require("../../controllers/view/publicRender");
 
 const router = Router();
 
-router.use(authenticate, privateViews);
 router.use(publicViews);
+router.use(authenticate, privateViews);
+router.get("*", renderHomePage);
 
 module.exports = router;
--- src/routes/view/privateViews.js
+++ src/routes/view/privateViews.js
@@ -14,12 +14,12 @@ const {
 const router = Router();
 
 //private
-router.get("/dashboard", renderDashboardPage);
-router.get("/quiz", renderMainQuizPage);
+router.get("/dashboard*", renderDashboardPage);
 router.get("/quiz/search", renderSearchedQuizzes);
 router.get("/quiz/create", renderCreateQuizPage);
 router.get("/quiz/create/question", renderCreateQuestionPage);
 router.get("/quiz/generate", renderGenerateQuiz);
 router.get("/quiz/:id", renderQuizPageById);
+router.get("/quiz*", renderMainQuizPage);
 
 module.exports = router;
